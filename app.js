557df0d78834c181c863fe9108abf7fe8fff416e
SRINIDHI <srinidhipathy@gmail.com>
2020-07-18 08:11:07 +0530

    'commented'

--- app.js
+++ app.js
@@ -20,49 +20,49 @@ script.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'tic_tac_toe.html'));
 });
 
-script.get('/:username',(req,res)=>{
-   const username=req.params.username;
-db.getDB().collection(collection).find({Name:username}).toArray((err,documents)=>{
-    if(err){
-        console.log(err);
-    }else{
-        res.json(documents);
-        console.log(documents);
-    }
-});
-});
+// script.get('/:username',(req,res)=>{
+//    const username=req.params.username;
+// db.getDB().collection(collection).find({Name:username}).toArray((err,documents)=>{
+//     if(err){
+//         console.log(err);
+//     }else{
+//         res.json(documents);
+//         console.log(documents);
+//     }
+// });
+// });
     
-script.put('/:id',(req,res)=>{
-const userID=req.params.id;
-const userInput=req.body;
-db.getDB().collection(collection).findOneAndUpdate({_id:db.getPrimaryKey(userID)},{$set:{Time:userInput}},{returnOriginal:false},(err,result)=>{
-    if(err)
-    console.log(err);
-    else
-    res.json(result);
-});
-});
+// script.put('/:id',(req,res)=>{
+// const userID=req.params.id;
+// const userInput=req.body;
+// db.getDB().collection(collection).findOneAndUpdate({_id:db.getPrimaryKey(userID)},{$set:{Time:userInput}},{returnOriginal:false},(err,result)=>{
+//     if(err)
+//     console.log(err);
+//     else
+//     res.json(result);
+// });
+// });
 
 
 
-script.post('/',(req,res)=>{
-    const userInput=req.body;
-    db.getDB().collection(collection).insertOne(userInput,(err,result)=>{
-        if(err)
-        console.log(err);
-        else{
-            res.json({result:result,document:result.ops[0]});
-        }
-    });
-});
+// script.post('/',(req,res)=>{
+//     const userInput=req.body;
+//     db.getDB().collection(collection).insertOne(userInput,(err,result)=>{
+//         if(err)
+//         console.log(err);
+//         else{
+//             res.json({result:result,document:result.ops[0]});
+//         }
+//     });
+// });
 
-script.delete('/:id',(req,res)=>{
-const userID=req.params.id;
-db.getDB().collection(collection).findOneAndDelete({_id:db.getPrimaryKey(userID)},(err,result)=>{
-if(err)
-console.log(err);
-else{
-    res.json(result);
-}
-});
-});
+// script.delete('/:id',(req,res)=>{
+// const userID=req.params.id;
+// db.getDB().collection(collection).findOneAndDelete({_id:db.getPrimaryKey(userID)},(err,result)=>{
+// if(err)
+// console.log(err);
+// else{
+//     res.json(result);
+// }
+// });
+// });
